43eb6e7e7bf0321f431cc342616326534496bade
lauearo <earo.lau@outlook.com>
2017-05-02 14:24:15 +0800

    first Spring boot page

--- src/main/java/com/earo/test/SpringBootDemoApplication.java
+++ src/main/java/com/earo/test/SpringBootDemoApplication.java
@@ -2,9 +2,17 @@ package com.earo.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication	//Spring Boot core annotation
 public class SpringBootDemoApplication {
+
+	@RequestMapping("/")
+	public String index(){
+		return "Hello spring boot";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
