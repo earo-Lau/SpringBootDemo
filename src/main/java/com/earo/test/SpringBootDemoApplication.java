a449401aeb8efad207db30057e6cc974c5efdfc0
lauearo <earo.lau@outlook.com>
2017-05-03 12:00:22 +0800

    auto configuration demo

--- src/main/java/com/earo/test/SpringBootDemoApplication.java
+++ src/main/java/com/earo/test/SpringBootDemoApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication	//Spring Boot core annotation
+//@SpringBootApplication	//Spring Boot core annotation
 public class SpringBootDemoApplication {
 
     @Value("${project.auth}")
