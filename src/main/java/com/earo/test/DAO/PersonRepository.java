b28118f410820c235b63848ffd68fe932229166e
lauearo <earo.lau@outlook.com>
2017-05-11 18:45:18 +0800

    Rest service demo

--- src/main/java/com/earo/test/DAO/PersonRepository.java
+++ src/main/java/com/earo/test/DAO/PersonRepository.java
@@ -4,18 +4,24 @@ import com.earo.test.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 /**
  * Created by lauearo on 09/05/2017.
  */
+@RepositoryRestResource(path = "people")
 public interface PersonRepository extends CustomRepository<Person, Long> {
     List<Person> findByAddress(String address);
-    Person findByNameAndAddress(String name, String address);
+    @RestResource(path = "byNameAndAddress", rel = "byNameAndAddress")
+    Person findByNameAndAddress(@Param("name") String name, @Param("address") String address);
 
     @Query(value = "select p from Person p where p.name= :name and p.address= :address")
     Person withNameAndAddressQuery(@Param("name") String name, @Param("address") String address);
-
     List<Person> withNameAndAddressNameQuery(String name, String address);
+
+    @RestResource(path = "nameStartsWith", rel = "nameStartsWith")
+    Person findByNameStartsWith(@Param("name") String name);
 }
--- src/main/java/com/earo/test/WebSecurityConfig.java
+++ src/main/java/com/earo/test/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(http);
-
+        http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/**").permitAll()  //permit "/" and "/login" request
                 .anyRequest().authenticated()
